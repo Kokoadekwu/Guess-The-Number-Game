2f3771380af7158daffae5d8cbb7937a339f5491
Kokoadekwu <119619652+Kokoadekwu@users.noreply.github.com>
2023-05-04 17:44:07 +0100

    Add files via upload

---
+++ login.js
@@ -0,0 +1,7 @@
+// Create the login function here.
+function login() {
+    playername = document.getElementById("playername").value;
+    localStorage.setItem("playername", playername);
+
+    window.location = "gamepage.html";
+}
